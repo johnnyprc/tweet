535d07e6f5537c0e361d2a589b867eb3fee5f847
johnnyprc <johnnyprc@live.cn>
2016-06-25 17:55:40 -0700

    changed indentation to all spaces

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -8,23 +8,23 @@ use Illuminate\Http\Request as Request;
 
 class UserController extends Controller
 {
-	function index()
-	{
+    function index()
+    {
 
-		return view('users.tweet');
-		// return View::make('../../welcome');
-	}
+        return view('users.tweet');
+        // return View::make('../../welcome');
+    }
 
-	public function tweet(Request $request)
-	{
-		$tweetText = $request->input('tweetText');
-		$connection = new TwitterOAuth(getenv('TWITTER_CLIENT_ID'),
+    public function tweet(Request $request)
+    {
+        $tweetText = $request->input('tweetText');
+        $connection = new TwitterOAuth(getenv('TWITTER_CLIENT_ID'),
                                        getenv('TWITTER_CLIENT_SECRET'),
                                        getenv('TWITTER_ACCESS_TOKEN'),
                                        getenv('TWITTER_ACCESS_TOKEN_SECRET'));  
         $connection->host = 'https://api.twitter.com/1.1/'; 
         $statues = $connection->post("statuses/update", ["status" => $tweetText]); 
-		echo "data: " . $tweetText;
-		return view('welcome');
-	}
+        echo "data: " . $tweetText;
+        return view('welcome');
+    }
 }
